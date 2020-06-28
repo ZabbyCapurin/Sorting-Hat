cddfae3734285954c3d09268defa1ea20d4c5e1e
Zabby Capurin <zabby.topacio@gmail.com>
2020-06-28 08:11:40 -0700

    WIP

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,10 @@ const del = require('del');
 const runSequence = require('run-sequence');
 const webpackStream = require('webpack-stream');
 const react = require('gulp-react');
+const ghpages = require('gh-pages');
 const webpackConfig = require('./webpack.config');
+
+ghpages.publish('dist', (err) => { });
 
 gulp.task('browserSync', () => {
   browserSync.init({
@@ -36,35 +39,45 @@ gulp.task('react', () => gulp.src('src/templates/*.jsx')
   .pipe(browserSync.reload({
     stream: true,
   })));
+
 gulp.task('copy', () => gulp.src('src/*.html')
   .pipe(gulp.dest('dist'))
   .pipe(browserSync.reload({
     stream: true,
   })));
 
+gulp.task('clean:dist', (done) => del(['dist/**/*', '!dist/images', '!dist/images/**/*'], done()));
+
 gulp.task('watch', () => {
-  gulp.watch('src/styles/*.scss', ['sass']);
-  gulp.watch('src/scripts/**/*.js', ['webpackStream']);
-  gulp.watch('src/templates/*.jsx', ['react']);
-  gulp.watch('src/*.html', ['copy']);
+  gulp.watch('src/styles/*.scss', gulp.series('sass'));
+  gulp.watch('src/scripts/**/*.js', gulp.series('webpackStream'));
+  gulp.watch('src/templates/*.jsx', gulp.series('react'));
+  gulp.watch('src/*.html', gulp.series('copy'));
 });
 
-gulp.task('clean:dist', () => del.sync(['dist/**/*', '!dist/images', '!dist/images/**/*']));
+// gulp.task('default', (done) => {
+//   gulp.series(
+//     gulp.parallel(
+//       'sass',
+//       'react',
+//       'webpackStream',
+//     ),
+//     'copy',
+//     'watch',
+//     done,
+//   );
+// });
 
-gulp.task('default', (callback) => {
-  runSequence(
-    ['sass', 'react', 'webpackStream', 'browserSync'], 'copy', 'watch',
-    callback,
-  );
-});
-
-gulp.task('build', (callback) => {
-  runSequence(
+gulp.task('build',
+  gulp.series(
     'clean:dist',
-    'sass',
-    'react',
-    'webpackStream',
-    ['copy'],
-    callback,
-  );
-});
+    gulp.parallel(
+      'sass',
+      'react',
+      'webpackStream',
+      // 'browserSync',
+    ),
+    // 'copy',
+  ));
+
+gulp.task('default', gulp.series('build'));
